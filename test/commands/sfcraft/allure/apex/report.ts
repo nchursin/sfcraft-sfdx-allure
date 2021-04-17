8be9b4b8ca8470d669073c39057cbb946f0201da
Nikita Chursin <chursinn91@gmail.com>
2021-04-17 13:20:45 +0300

    empty test file

---
+++ test/commands/sfcraft/allure/apex/report.ts
@@ -0,0 +1,6 @@
+import { ensureJsonMap, ensureString } from "@salesforce/ts-types";
+import expect from "chai";
+
+describe("sfcraft:allure:apex:report", () => {
+  it("calls force:apex:test:report to get report", () => {});
+});
