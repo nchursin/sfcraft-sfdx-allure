03c87e2817ce00265fdcef3f71fb9a197dec3a4e
Nikita Chursin <chursinn91@gmail.com>
2021-04-17 15:48:52 +0300

    replaced mocha with jest

--- test/commands/sfcraft/allure/apex/report.ts
+++
@@ -1,6 +0,0 @@
-import { ensureJsonMap, ensureString } from "@salesforce/ts-types";
-import expect from "chai";
-
-describe("sfcraft:allure:apex:report", () => {
-  it("calls force:apex:test:report to get report", () => {});
-});
